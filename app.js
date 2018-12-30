d650bf0bb1bfcc6875b71f4d92ba3a300e625e3d
Nabeel Salam <nabeelsalam00@gmail.com>
2018-12-29 23:17:45 -0500

    Loading all urls as https

--- app.js
+++ app.js
@@ -103,6 +103,7 @@ async function createNewsList(source) {
  *  Creating the news card
  */
 function createCard({title, description, urlToImage, url}) {
+  url = url.replace('http', 'https');
   return `
   <a
   data-fancybox
